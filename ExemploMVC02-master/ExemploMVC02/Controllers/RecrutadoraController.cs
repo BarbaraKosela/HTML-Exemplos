f86e84fb2a36d05742f54f5d4dc11bb11fefe9ab
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-08-03 14:20:25 -0300

    up

---
+++ ExemploMVC02-master/ExemploMVC02/Controllers/RecrutadoraController.cs
@@ -0,0 +1,42 @@
+﻿using ExemploMVC02.Models;
+using ExemploMVC02.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploMVC02.Controllers
+{
+    public class RecrutadoraController : Controller
+    {
+        // GET: Recrutadora
+        public ActionResult Index()
+        {
+            List<Recrutadora> recrutadoras = new RecrutadoraRepositorio().ObterTodos();
+            ViewBag.Recrutadoras = recrutadoras;
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Store(Recrutadora recrutadora)
+        {
+            int id = new RecrutadoraRepositorio().Cadastrar(recrutadora);
+
+
+            return null;
+        }
+
+        public ActionResult Update()
+        {
+            return null;
+        }
+    }
+}
---
+++ ExemploMVC02-master/ExemploMVC02/Repositorio/RecrutadoraRepositorio.cs
@@ -0,0 +1,62 @@
+﻿using ExemploMVC02.Database;
+using ExemploMVC02.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ExemploMVC02.Repositorio
+{
+    public class RecrutadoraRepositorio
+    {
+        public List<Recrutadora> ObterTodos()
+        {
+           List<Recrutadora> recrutadoras = new List<Recrutadora>();
+           SqlCommand comando = new BancoDados().ObterConexcao();
+           comando.CommandText = "SELECT id,nome,cpf,tempo_empresa,salario FROM recrutadoras";
+           DataTable tabela = new DataTable();
+           tabela.Load(comando.ExecuteReader());
+           foreach (DataRow linha in tabela.Rows)
+           {
+               Recrutadora recrutadora = new Recrutadora()
+               {
+                   Id = Convert.ToInt32(linha[0].ToString()),
+                   Nome = linha[1].ToString(),
+                   CPF = linha[2].ToString(),
+                   TempoEmpresa = Convert.ToByte(linha[3].ToString()),
+                   Salario = Convert.ToDecimal(linha[4].ToString())
+               };
+               recrutadoras.Add(recrutadora);
+           }
+            return recrutadoras;
+        }
+        public int Cadastrar(Recrutadora recrutadora)
+        {
+            SqlCommand comando = new BancoDados().ObterConexcao();
+            comando.CommandText = @"INSERT INTO recrutadoras (nome,salario,cpf,tempo_empresa) OUTPUT INSERTED.ID VALUES (@NOME,@SALARIO,@CPF,@TEMPO_EMPRESA)";
+            comando.Parameters.AddWithValue("@NOME", recrutadora.Nome);
+            comando.Parameters.AddWithValue("@SALARIO", recrutadora.Salario);
+            comando.Parameters.AddWithValue("@CPF", recrutadora.CPF);
+            comando.Parameters.AddWithValue("@TEMPO_EMPRESA", recrutadora.TempoEmpresa);
+            int id = Convert.ToInt32(comando.ExecuteScalar().ToString());
+            return id;
+        }
+
+        public bool Alterar(Recrutadora recrutadora)
+        {
+            return false;
+        }
+
+        public bool Excluir(int id)
+        {
+            return false;
+        }
+        public Recrutadora ObterPeloId(int id)
+        {
+            Recrutadora recrutadora = null;
+            return recrutadora;
+        }
+    }
+}
