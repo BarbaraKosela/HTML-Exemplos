4930c95a5597669b3d29b804a320e126cbfe58cc
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-08-06 17:25:50 -0300

    .

--- ExemploMVC02-master/ExemploMVC02/Scripts/Recrutadora/cadastro.js
+++ ExemploMVC02-master/ExemploMVC02/Scripts/Recrutadora/cadastro.js
@@ -2,6 +2,7 @@
     var textoCampoNome = document.getElementById("campo-nome").value;
     var textoCapoCPF = document.getElementById("campo-cpf").value;
     var textoCampoEmpresa = document.getElementById("campo-tempo-empresa").value;
+    var textoCampoSalario = document.getElementById("campo-salario").value;
 
     var quantidaddeCaracteresCampoNome = textoCampoNome.lenght;
     console.log("Texto: " + quantidaddeCaracteresCampoNome)
@@ -10,4 +11,18 @@
     if (quantidaddeCaracteresCampoNome > 10 || quantidaddeCaracteresCampoNome < 100) {
         alert("Campo nome deve ter no mínimo 10 caracteres e no máximo 100 caracteres");
     }
+
+    var quantidadeCaractereCampoCPF = textoCapoCPF.lenght;
+    if (quantidadeCaractereCampoCPF < 15) {
+        alert("Campo de CPF deve conter 16 dígitos");
+
+    }
+    var salario = parseFloat(textoCampoSalario);
+    if (salario < 1200) {
+        alert("Salário deve ser maior que 1200");
+    }
+    else if (salario > 100000) {
+        alert("Salário não pode ultrapassar 100000");
+    }
+    
 }
