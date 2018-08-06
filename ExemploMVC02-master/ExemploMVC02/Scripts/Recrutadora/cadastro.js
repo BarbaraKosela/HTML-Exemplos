d3f04d47fd029f5b4a844b667081ed947c688570
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-08-06 17:03:47 -0300

    Js

---
+++ ExemploMVC02-master/ExemploMVC02/Scripts/Recrutadora/cadastro.js
@@ -0,0 +1,13 @@
+﻿function validarFormulario() {
+    var textoCampoNome = document.getElementById("campo-nome").value;
+    var textoCapoCPF = document.getElementById("campo-cpf").value;
+    var textoCampoEmpresa = document.getElementById("campo-tempo-empresa").value;
+
+    var quantidaddeCaracteresCampoNome = textoCampoNome.lenght;
+    console.log("Texto: " + quantidaddeCaracteresCampoNome)
+    console.log("Quantidade: " + textoCampoNome);
+
+    if (quantidaddeCaracteresCampoNome > 10 || quantidaddeCaracteresCampoNome < 100) {
+        alert("Campo nome deve ter no mínimo 10 caracteres e no máximo 100 caracteres");
+    }
+}
