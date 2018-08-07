f75ff643142be0fcee46304b2a3f1899552fb6fe
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-08-07 15:52:36 -0300

    Não deu certo

--- ExemploMVC02-master/ExemploMVC02/Scripts/Recrutadora/cadastro.js
+++ ExemploMVC02-master/ExemploMVC02/Scripts/Recrutadora/cadastro.js
@@ -1,5 +1,5 @@
 ﻿
-function apagarElemento()
+function apagarElemento(id)
 {
     if (document.contains(document.getElementById(id)))
     {
@@ -7,45 +7,19 @@ function apagarElemento()
     }
 }
 
-function apagarElementoDeValidacao() {
-    apagarElemento("span-campo-nome-menor-10");
-    apagarElemento("span-campo-nome-maior-100")
-}
+
 
 function validarFormulario() {
-    var textoCampoNome = document.getElementById("campo-nome").value;
     var textoCapoCPF = document.getElementById("campo-cpf").value;
     var textoCampoSalario = document.getElementById("campo-salario").value;
-    var textoCampoTempoEmpresa = document.getElementById("campo-tempo-empresa").value;
+    if (validarCampoNome() == false) {
+        event.preventDefault();
+    }
 
-    apagarElementoDeValidacao();
-
+    validarCampoNome();
+    
    
-    if (textoCampoNome.lenght < 10) {
-        
-
-            var elementoSpanNome = document.createElement("span");
-            var texto = document.createTextNode("Campo nome deve ter no mínimo 10 caracteres");
-            elementoSpanNome.id = "span-campo-nome-menor-10";
-            elementoSpanNome.appendChild(texto);
-            elementoSpanNome.classList.add("text-danger");
-            document.getElementById("div-campo-nome").appendChild(elementoSpanNome);
-            document.getElementById("campo-nome").classList.add("border-danger");
-        
-    }
-
-    if (textoCampoNome.lenght > 100) {
-
-        elementoSpanNome = document.createElement("span");
-        var texto = document.createTextNode("Campo nome deve ter no máximo 100 caracteres");
-        elementoSpanNome.id = "span-campo-nome-maior-100";
-        elementoSpanNome.appendChild(texto);
-        elementoSpanNome.classList.add("text-danger");
-        document.getElementById("div-campo-nome").appendChild(elementoSpanNome);
-        document.getElementById("campo-nome").classList.add("border-danger");
-
-
-    }
+    
 
     /*var quantidadeCaractereCampoCPF = textoCapoCPF.lenght;
     if (quantidadeCaractereCampoCPF < 15) {
@@ -60,4 +34,46 @@ function validarFormulario() {
         alert("Salário não pode ultrapassar 100000");
     }*/
     
+}
+
+function validarCampoNome() {
+
+    var textoCampoNome = document.getElementById("campo-nome").value;
+
+    document.getElementById("campo-nome").classList.remove("border-sucess");
+
+    apagarElemento("span-campo-nome-menor-10");
+    apagarElemento("span-campo-nome-maior-100")
+
+    if (textoCampoNome.lenght < 10) {
+
+
+        var elementoSpanNome = document.createElement("span");
+        var texto = document.createTextNode("Campo nome deve ter no mínimo 10 caracteres");
+        elementoSpanNome.id = "span-campo-nome-menor-10";
+        elementoSpanNome.appendChild(texto);
+        elementoSpanNome.classList.add("text-danger");
+        document.getElementById("div-campo-nome").appendChild(elementoSpanNome);
+        document.getElementById("campo-nome").classList.add("border-danger");
+        return false;
+    }
+
+    if (textoCampoNome.lenght > 100) {
+
+        elementoSpanNome = document.createElement("span");
+        var texto = document.createTextNode("Campo nome deve ter no máximo 100 caracteres");
+        elementoSpanNome.id = "span-campo-nome-maior-100";
+        elementoSpanNome.appendChild(texto);
+        elementoSpanNome.classList.add("text-danger");
+        document.getElementById("div-campo-nome").appendChild(elementoSpanNome);
+        document.getElementById("campo-nome").classList.add("border-danger");
+        return false;
+
+    }
+
+    if (textoCampoNome.lenght >= 7 && textoCampoNome.lenght <= 100) {
+        document.getElementById("campo-nome").classList.remove("border-danger");
+        document.getElementById("campo-nome").classList.add("border-sucess");
+        return true;
+    }
 }
